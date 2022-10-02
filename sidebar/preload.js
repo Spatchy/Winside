75f1aa446594abdd8ca9db800ab9a1465e0d2ab3
Spatchy <spatchy@spatchy.net>
2022-10-02 15:28:21 +0200

    implemented sidebar control panel and close button

--- sidebar/preload.js
+++ sidebar/preload.js
@@ -1,10 +1,17 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
-  }
+const { contextBridge, ipcRenderer } = require("electron")
 
+contextBridge.exposeInMainWorld("WinsideAPI", {
+  closeSidebar: () => ipcRenderer.send("closeSidebar")
+})
+
+const replaceText = (selector, text) => {
+  const element = document.getElementById(selector)
+  if (element) element.innerText = text
+}
+  
+window.addEventListener("DOMContentLoaded", () => {
   for (const dependency of ["chrome", "node", "electron"]) {
     replaceText(`${dependency}-version`, process.versions[dependency])
   }
+
 })
