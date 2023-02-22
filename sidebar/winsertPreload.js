f4e25f6532b671ae3713237aaf64fc3ca2453beb
Spatchy <spatchy@spatchy.net>
2023-02-22 23:03:07 +0100

    removed closeSidebar Winsert endpoint for security

--- sidebar/winsertPreload.js
+++ sidebar/winsertPreload.js
@@ -1,8 +1,6 @@
 const { contextBridge, ipcRenderer } = require("electron")
 
 contextBridge.exposeInMainWorld("WinsideAPI", {
-  closeSidebar: () => ipcRenderer.send("closeSidebar"),
-
   openLinkInBrowser: (link) => ipcRenderer.send("openLinkInBrowser", link),
 
   requestPermission: (permissionName) => {
