012f8e32197df2cc4bea43f7acf30d40f69207ac
Spatchy <spatchy@spatchy.net>
2023-03-01 22:34:46 +0100

    Update forge config

--- forge.config.js
+++ forge.config.js
@@ -3,20 +3,8 @@ module.exports = {
   rebuildConfig: {},
   makers: [
     {
-      name: '@electron-forge/maker-squirrel',
+      name: "@electron-forge/maker-squirrel",
       config: {},
-    },
-    {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
-    },
-    {
-      name: '@electron-forge/maker-deb',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {},
-    },
+    }
   ],
-};
+}
