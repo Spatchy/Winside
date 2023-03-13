857acf808588d80652262d03e0246f8d5ce123d2
Spatchy <spatchy@spatchy.net>
2023-03-13 21:51:28 +0100

    Add squirrel icons

--- forge.config.js
+++ forge.config.js
@@ -1,10 +1,16 @@
+/* eslint-disable max-len */
+const path = require("path")
+
 module.exports = {
   packagerConfig: {},
   rebuildConfig: {},
   makers: [
     {
       name: "@electron-forge/maker-squirrel",
-      config: {},
+      config: {
+        iconUrl: "https://raw.githubusercontent.com/Spatchy/Winside/main/logo.ico",
+        setupIcon: path.join(__dirname, "logo.ico")
+      },
     }
   ],
 }
